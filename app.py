c0d512c1792b8257974acaf05f24b6d6af8eea3d
Andy Wu <andyww09@gmail.com>
2023-10-11 19:39:36 -0700

    still running into issue with tables

--- app.py
+++ app.py
@@ -1,34 +1,69 @@
 import streamlit as st
-from langchain.chains import RetrievalQA
 from langchain.document_loaders import TextLoader
-from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.llms import OpenAI
-from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
-
+from langchain.indexes import VectorstoreIndexCreator
+import os 
+from langchain.prompts import PromptTemplate
+import constants
 from dotenv import load_dotenv
+from langchain.chains import LLMChain, SequentialChain
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
 load_dotenv()
 
+os.environ['OPENAI_API_KEY'] = constants.OPENAI_API_KEY
+
 st.title("Andy's Chatbot")
-prompt = st.text_input("Ask me anything:")
+question = st.text_input("Ask me anything:")
 
-loader = TextLoader("about_me.txt")
-documents = loader.load()
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-texts = text_splitter.split_documents(documents)
+file_path = "about_me.txt"
+loader = TextLoader(file_path)
+docs = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=0)
 
+prompt_template = """
+Use the following pieces of context to answer the question at the end.
+{context}
+Question: {question}
+Answer:"""
+prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+
+llm = OpenAI()
 embeddings = OpenAIEmbeddings()
-docsearch = Chroma.from_documents(texts, embeddings)
+texts = text_splitter.split_documents(docs)
+db = Chroma.from_documents(texts, embeddings)
 
-qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever())
+# question = "What is your name?"
+similar_doc = db.similarity_search(question, k=1)
+context = similar_doc[0].page_content
+query_llm = LLMChain(llm=llm, prompt=prompt)
 
+# loader = TextLoader("about_me.txt")
+# index = VectorstoreIndexCreator().from_loaders([loader])
+
+response = query_llm.run({"context": context, "question": question})
+# print("response", response)
+# 
+# print(index.query("Tell me about your school"))
+
+# response = index.query(prompt)
 if st.button('Generate'):
-    if prompt:
-        # print("check")
-        with st.spinner('Generating response...'):
-            qa.run(prompt)
-    else:
-        st.warning('Please enter your prompt')
+    if question:
+        st.write(response)
+        # this_prompt = "tell me about"
+        # print("response", response)
+        # response = query_llm.run({"context": context, "question": question})
+        # st.write(response)
+#         st.write(response)
+        # index.query(prompt)
+        # st.write(index.query(prompt))
+        # with st.spinner('Generating response...'):
+            # index.query(prompt)
+            # print("response", response)
+            # st.write(response)
+    # else:
+    #     st.warning('Please enter your prompt')
 
 
 
